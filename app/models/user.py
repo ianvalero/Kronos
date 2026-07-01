6a4cd2413bfc96284ca9f51562a7710c3bd8cb63
Ian Valero <ian.valero-gadea@telekom.com>
2026-07-01 15:55:53 +0200

    alembic

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,6 @@
 from sqlmodel import SQLModel, Field
 
-class User(SQLModel, table=True):
+class UserDB(SQLModel, table=True):
     __tablename__ = "users"
     id: str | None = Field(default=None, primary_key=True)
     role: str
